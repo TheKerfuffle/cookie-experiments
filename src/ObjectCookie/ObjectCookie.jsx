e3b77f2445dc213e5264e790e9ca834431f2cfd1
Charlie Stokes <chstokes7@gmail.com>
2021-06-15 16:38:04 -0500

    added dropdown which works with universal function

--- src/ObjectCookie/ObjectCookie.jsx
+++ src/ObjectCookie/ObjectCookie.jsx
@@ -10,17 +10,21 @@ function ObjectCookie(props) {
     const [cookies, setCookie, removeCookie] = useCookies(['user']);
     let [localCookies, setLocalCookies] = useState(cookies);
 
-    let [inputthing, setinputthing] = useState('');
+    let [removeThing, setRemoveThing] = useState('');
 
 
     useEffect(() => {
-        if (cookies.patientName === undefined) {
-            setCookie('patientName', '', { path: '/' });
+        if (cookies.patientFirstName === undefined) {
+            setCookie('patientFirstName', '', { path: '/' });
         }
         if (cookies.patientLastName === undefined) {
             setCookie('patientLastName', '', { path: '/' });
         }
-        
+        if (cookies.bloodType === undefined) {
+            setCookie('bloodType', '', { path: '/' });
+        }
+
+
 
     }, [cookies])
 
@@ -31,7 +35,7 @@ function ObjectCookie(props) {
     function submitCookie(newCookie) {
         console.log('setting THE cookie', newCookie.key, newCookie.value);
 
-        setLocalCookies({...localCookies, [newCookie.key]: newCookie.thing});
+        setLocalCookies({ ...localCookies, [newCookie.key]: newCookie.thing });
 
         setCookie(newCookie.key, newCookie.thing, { path: '/' });
     }
@@ -52,66 +56,56 @@ function ObjectCookie(props) {
 
     return (
         <>
+            {/* Display Global cookie and Local cookie object  */}
             <p>ObjectCookie So Far:</p>
 
-            <p>{inputthing}</p>
+            <p>Thing to Remove: {removeThing}</p>
 
             <p>Local Cookie Object: {renderLocalCookie()}</p>
             <p>THE Cookie Object: {renderTHECookie()}</p>
 
-            {/* Input for the key part of a new cookie object */}
-            {/* <input
-                placeholder="Enter new cookie key"
-                type="text"
-                value={cookieKey}
-                onChange={(e) => setCookieKey(e.target.value)}>
-            </input> */}
-            {/* Input for the value part of a new cookie object */}
-            {/* <input
-                placeholder="Enter new cookie value"
-                type="text"
-                value={name}
-                onChange={(e) => setinputthing(e.target.value)}>
-            </input>
-            <button onClick={() => submitCookie(cookieKey, name)}>
-                Butthole
-            </button> */}
             <p>Patient First Name:</p>
             <input
                 placeholder="Enter THE cookie value"
                 type="text"
-                value={localCookies.patientName}
-                onChange={(e) => submitCookie({key: 'patientName', thing: e.target.value})}>
+                value={localCookies.patientFirstName}
+                onChange={(e) => submitCookie({ key: 'patientFirstName', thing: e.target.value })}>
             </input>
-
-            {/* <input
-                placeholder="Enter local cookie value"
-                type="text"
-                value={localCookies.patientName}
-                onChange={(e) => setLocalCookies({...localCookies, patientName: e.target.value})}>
-            </input> */}
 
             <p>Patient Last Name:</p>
             <input
                 placeholder="Enter THE cookie value"
                 type="text"
                 value={localCookies.patientLastName}
-                onChange={(e) => submitCookie({key: 'patientLastName', thing: e.target.value})}>
+                onChange={(e) => submitCookie({ key: 'patientLastName', thing: e.target.value })}>
             </input>
 
-            {/* <button onClick={() => submitCookie("patientName", cookies.patientName)}>
-                Change Patient Name 
-            </button> */}
+            <p>Patient Blood Type:</p>
+            <select
+                name="Blood Type"
+                value={localCookies.bloodType}
+                onChange={(e) => submitCookie({ key: 'bloodType', thing: e.target.value })}
+            >
+                <option value="">--- Select Blood Type --- </option>
+                <option value="O+">O Positive</option>
+                <option value="O-">O Negative</option>
+                <option value="A+">A Positive</option>
+                <option value="A-">A Negative</option>
+                <option value="B+">B Positive</option>
+                <option value="B-">B Negative</option>
+                <option value="AB+">AB Positive</option>
+                <option value="AB-">AB Negative</option>
+            </select>
 
 
             <p>Remove Cookie:</p>
             <input
                 placeholder="Enter new cookie value"
                 type="text"
-                value={inputthing}
-                onChange={(e) => setinputthing(e.target.value)}>
+                value={removeThing}
+                onChange={(e) => setRemoveThing(e.target.value)}>
             </input>
-            <button onClick={() => removeCookie(inputthing)}>
+            <button onClick={() => removeCookie(removeThing)}>
                 Remove Cookie
             </button>
 
